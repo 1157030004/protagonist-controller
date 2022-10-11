955ce77ea815fc5e0b743d88e33e22fdca05e41a
Ariq Dhia Irfanudin <62055087+1157030004@users.noreply.github.com>
2022-10-12 05:24:35 +0700

    add camera cinemachine

--- Assets/InputActions/Characters/Protagonist/GameInput.cs
+++ Assets/InputActions/Characters/Protagonist/GameInput.cs
@@ -44,6 +44,24 @@ public partial class @GameInput : IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""Value"",
+                    ""id"": ""97582183-ee2b-4789-980e-853bfa46da9c"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Zoom"",
+                    ""type"": ""Value"",
+                    ""id"": ""6f1f7d2b-43f6-4240-b21d-60e2fdd7a25b"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": ""Clamp(min=-0.1,max=0.1),Invert"",
+                    ""interactions"": """",
+                    ""initialStateCheck"": true
                 }
             ],
             ""bindings"": [
@@ -123,6 +141,28 @@ public partial class @GameInput : IInputActionCollection2, IDisposable
                     ""action"": ""WalkToggle"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d5e62097-988e-49dd-a1f6-a2f2a83769c1"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""2de000fc-89ab-461c-9f8e-f8ae3261d9a5"",
+                    ""path"": ""<Mouse>/scroll/y"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Zoom"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -133,6 +173,8 @@ public partial class @GameInput : IInputActionCollection2, IDisposable
         m_Protagonist = asset.FindActionMap("Protagonist", throwIfNotFound: true);
         m_Protagonist_Movement = m_Protagonist.FindAction("Movement", throwIfNotFound: true);
         m_Protagonist_WalkToggle = m_Protagonist.FindAction("WalkToggle", throwIfNotFound: true);
+        m_Protagonist_Look = m_Protagonist.FindAction("Look", throwIfNotFound: true);
+        m_Protagonist_Zoom = m_Protagonist.FindAction("Zoom", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -194,12 +236,16 @@ public partial class @GameInput : IInputActionCollection2, IDisposable
     private IProtagonistActions m_ProtagonistActionsCallbackInterface;
     private readonly InputAction m_Protagonist_Movement;
     private readonly InputAction m_Protagonist_WalkToggle;
+    private readonly InputAction m_Protagonist_Look;
+    private readonly InputAction m_Protagonist_Zoom;
     public struct ProtagonistActions
     {
         private @GameInput m_Wrapper;
         public ProtagonistActions(@GameInput wrapper) { m_Wrapper = wrapper; }
         public InputAction @Movement => m_Wrapper.m_Protagonist_Movement;
         public InputAction @WalkToggle => m_Wrapper.m_Protagonist_WalkToggle;
+        public InputAction @Look => m_Wrapper.m_Protagonist_Look;
+        public InputAction @Zoom => m_Wrapper.m_Protagonist_Zoom;
         public InputActionMap Get() { return m_Wrapper.m_Protagonist; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -215,6 +261,12 @@ public partial class @GameInput : IInputActionCollection2, IDisposable
                 @WalkToggle.started -= m_Wrapper.m_ProtagonistActionsCallbackInterface.OnWalkToggle;
                 @WalkToggle.performed -= m_Wrapper.m_ProtagonistActionsCallbackInterface.OnWalkToggle;
                 @WalkToggle.canceled -= m_Wrapper.m_ProtagonistActionsCallbackInterface.OnWalkToggle;
+                @Look.started -= m_Wrapper.m_ProtagonistActionsCallbackInterface.OnLook;
+                @Look.performed -= m_Wrapper.m_ProtagonistActionsCallbackInterface.OnLook;
+                @Look.canceled -= m_Wrapper.m_ProtagonistActionsCallbackInterface.OnLook;
+                @Zoom.started -= m_Wrapper.m_ProtagonistActionsCallbackInterface.OnZoom;
+                @Zoom.performed -= m_Wrapper.m_ProtagonistActionsCallbackInterface.OnZoom;
+                @Zoom.canceled -= m_Wrapper.m_ProtagonistActionsCallbackInterface.OnZoom;
             }
             m_Wrapper.m_ProtagonistActionsCallbackInterface = instance;
             if (instance != null)
@@ -225,6 +277,12 @@ public partial class @GameInput : IInputActionCollection2, IDisposable
                 @WalkToggle.started += instance.OnWalkToggle;
                 @WalkToggle.performed += instance.OnWalkToggle;
                 @WalkToggle.canceled += instance.OnWalkToggle;
+                @Look.started += instance.OnLook;
+                @Look.performed += instance.OnLook;
+                @Look.canceled += instance.OnLook;
+                @Zoom.started += instance.OnZoom;
+                @Zoom.performed += instance.OnZoom;
+                @Zoom.canceled += instance.OnZoom;
             }
         }
     }
@@ -233,5 +291,7 @@ public partial class @GameInput : IInputActionCollection2, IDisposable
     {
         void OnMovement(InputAction.CallbackContext context);
         void OnWalkToggle(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
+        void OnZoom(InputAction.CallbackContext context);
     }
 }
